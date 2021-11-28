c7f9aa19566a54a2d5e3cb93ac2fb7662d2d99b1
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-11-28 17:47:24 -0500

    successful color changer

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
 extern crate image;
+use crate::image::Pixel;
 use image::Rgba;
 use std::str;
-use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage};
+use image::{GenericImage, GenericImageView, ImageBuffer, RgbaImage};
 
 fn main() {
     println!("Rust Profile Picture Generator");
     //let mut img: RgbImage = ImageBuffer::new(512, 512);
     //let mut img = ImageReader::open("static/head-shape/square.png").unwrap();
     //img.save("test.png").unwrap();
-    color_replace("static/head-shape/square.png");
+    color_replace("static/head/square.png", "output/head.png", "blue");
 }
 
-fn color_replace(img_location: &str) {
+fn color_replace(img_location: &str, final_loc: &str, color: &str) {
+    // add a hashmap for colors here
+    let new_col = Rgba([0,0,255,255]); // new col will equal the color corresponding to the hashmap
+
     let img = image::open(img_location).unwrap();
     let img_dim = img.dimensions(); // the dimensions of the image lol
-    let new_col = Rgba([255,0,0,0]);
+    let mut colored_img: RgbaImage = ImageBuffer::new(img_dim.0,img_dim.1);
+    let transparent = Rgba([255,255,255,0]);
+    
     for x in 0..(img_dim.0) {
         for y in 0..(img_dim.1) {
-            let mut pixel = img.get_pixel(x,y);
-            println!("{:?}",pixel);
-            if pixel == Rgba([255,255,255,0]) {
+            let mut pixel = img.get_pixel(x,y); 
+            if pixel != transparent {
                 pixel = new_col;
+                println!("({},{}), {:?}", x, y,pixel);
             }
-            println!("({},{}), {:?}", x, y,pixel);
-
-            //img.put_pixel(pixel.0.clone(), pixel.1.clone(), Rgba([255,0,0,0]));
-    
+            colored_img.put_pixel(x, y, pixel);
         }
     }
-    img.save("test.png");
+    colored_img.save(final_loc);
 }
